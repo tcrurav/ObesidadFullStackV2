6a696ff73b203e517bad4ce274cd392f59e53c49
Rocholl <55140973+Rocholl@users.noreply.github.com>
2021-03-09 18:27:42 +0100

    commit on master

--- BackEnd/app/config/config.js
+++ BackEnd/app/config/config.js
@@ -1,36 +1,35 @@
 
 require('dotenv').config();
 module.exports = {
-    development: {
-        username: "root",
-        password: null,
-        database: "obesidadSql",
-        host: "localhost",
-        dialect: 'mysql',
-        pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
-        seederStorage:"json",
-        seederStoragePath:"sequelizeSeeds.json",
-        migrationStorage:"sequelize",
-        define:{
-          timestamps:false,
+  development: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    pool: { max: 5, min: 0, acquire: 30000, idle: 10000 },
+    seederStorage: "json",
+    seederStoragePath: "sequelizeSeeds.json",
+    migrationStorage: "sequelize",
+    define: {
+      timestamps: false,
 
-        }
-      },
-      test: {
-        username: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE,
-        host: process.env.DB_HOST,
-        dialect: 'mysql',pool: { max: 5, min: 0, acquire: 30000, idle: 10000 }
-      },
-      production: {
-        username: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE,
-        host: process.env.DB_HOST,
-        dialect: 'mysql',
-        pool: { max: 5, min: 0, acquire: 30000, idle: 10000 }
-      }
-      
     }
-    
+  },
+  test: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'postgres', pool: { max: 5, min: 0, acquire: 30000, idle: 10000 }
+  },
+  production: {
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    pool: { max: 5, min: 0, acquire: 30000, idle: 10000 }
+  }
+
+}
